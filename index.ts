3fe4e4d6f110f56c576921a1305ee4e93ae1aee9
Miinggi <omiinggi@gmail.com>
2022-12-21 23:13:24 +0900

    added new class
    update: I have no idea how to make a search function or how to use sqlite :)
    
    Also added class search{ }to index.ts

--- index.ts
+++ index.ts
@@ -386,6 +386,17 @@ class App {
 }
 
 
-const app = new App();
+class search {
+    private db = new sqlite.Database('stock.db')
+    public searchStockName = async (stock_name: string, callback: (result: any) => void) => {
+        this.db.all('SELECT * FROM stock WHERE stock_name = ?', stock_name, (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                callback(result);
+            }
+        });
+    }
+}
 
 app.listen(8080)
