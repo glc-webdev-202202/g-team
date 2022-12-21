e95bf182849a845ee1b242a06839fa6be0e66f3b
zinc030 <ayeon.oh@yonsei.ac.kr>
2022-12-21 23:30:00 +0900

    Temporary fix (pls move search function)

--- index.ts
+++ index.ts
@@ -396,17 +396,19 @@ class App {
 }
 
 
-class search {
-    private db = new sqlite.Database('stock.db')
-    public searchStockName = async (stock_name: string, callback: (result: any) => void) => {
-        this.db.all('SELECT * FROM stock WHERE stock_name = ?', stock_name, (err, result) => {
-            if (err) {
-                console.log(err);
-            } else {
-                callback(result);
-            }
-        });
-    }
-}
+// class search {
+//     private db = new sqlite.Database('stock.db')
+//     public searchStockName = async (stock_name: string, callback: (result: any) => void) => {
+//         this.db.all('SELECT * FROM stock WHERE stock_name = ?', stock_name, (err, result) => {
+//             if (err) {
+//                 console.log(err);
+//             } else {
+//                 callback(result);
+//             }
+//         });
+//     }
+// }
+
+const app = new App();
 
 app.listen(8080)
